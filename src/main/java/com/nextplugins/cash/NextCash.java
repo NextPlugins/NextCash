696279e7577bc34f55ed5713d91152048202c2d3
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-01-26 15:53:41 -0300

    feat(sql): setup sql connection

--- src/main/java/com/nextplugins/cash/NextCash.java
+++ src/main/java/com/nextplugins/cash/NextCash.java
@@ -1,15 +1,22 @@
 package com.nextplugins.cash;
 
+import com.henryfabio.sqlprovider.connector.SQLConnector;
+import com.nextplugins.cash.sql.SQLProvider;
+import lombok.Getter;
 import me.bristermitten.pdm.PluginDependencyManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NextCash extends JavaPlugin {
 
+    @Getter private SQLConnector sqlConnector;
+
     @Override
     public void onEnable() {
         PluginDependencyManager.of(this).loadAllDependencies().thenRun(() -> {
             try {
+                sqlConnector = SQLProvider.of(this).setup();
+
                 getLogger().info("Plugin inicializado com sucesso.");
             } catch (Throwable t) {
                 t.printStackTrace();
