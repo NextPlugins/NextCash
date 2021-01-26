cf69d8e03f2a015a1bceada9bf99fece07ffae51
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-01-26 15:40:06 -0300

    Project structured

--- src/main/java/com/nextplugins/cash/NextCash.java
+++ src/main/java/com/nextplugins/cash/NextCash.java
@@ -1,17 +1,26 @@
 package com.nextplugins.cash;
 
+import me.bristermitten.pdm.PluginDependencyManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NextCash extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        PluginDependencyManager.of(this).loadAllDependencies().thenRun(() -> {
+            try {
+                getLogger().info("Plugin inicializado com sucesso.");
+            } catch (Throwable t) {
+                t.printStackTrace();
+                getLogger().severe("Ocorreu um erro durante a inicialização do plugin.");
+                Bukkit.getPluginManager().disablePlugin(this);
+            }
+        });
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public NextCash getInstance() {
+        return getPlugin(NextCash.class);
     }
+
 }
